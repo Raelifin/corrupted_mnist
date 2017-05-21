9244f161aa3f67e962a306e18f4cfd2b08643af1
Max Harms <raelifin@gmail.com>
2017-05-21 11:17:26 -0700

    Basic MNIST classifier working on challenge data

--- corrupted.py
+++ corrupted.py
@@ -25,16 +25,41 @@ from __future__ import print_function
 import argparse
 import sys
 
-from tensorflow.examples.tutorials.mnist import input_data
+# from tensorflow.examples.tutorials.mnist import input_data
+# pure_mnist = input_data.read_data_sets('/tmp/tensorflow/mnist/input_data', one_hot=True)
 
 import tensorflow as tf
+import numpy as np
 
 FLAGS = None
 
 
+def one_hot(hot_index, length):
+  return [1.0 if hot_index==i else 0.0 for i in range(length)]
+
+def import_data(filepath):
+  print("Reading data from %s..." % filepath)
+  xs = []
+  ys = []
+  with open(filepath) as datafile:
+    for line in datafile:
+      values = [float(x) for x in line.split(',')]
+      xs.append(values[1:])
+      ys.append(one_hot(values[0], 10))
+  return xs, ys
+
+def next_batch(data, start_location, samples):
+  # Use modulus to redraw from the beginning of the data if the start_location is too high.
+  start_location = start_location % len(data)
+  if start_location+samples > len(data):
+    # Concatinate a copy of the data onto the end to replicate redrawing from the beginning.
+    data = data + data
+    # This isn't very memory efficient, but I'm hacking a solution together. FIXME
+  return np.array(data[start_location:start_location+samples])
+
 def main(_):
   # Import data
-  mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+  images, labels = import_data(FLAGS.input_data)
 
   # Create the model
   x = tf.placeholder(tf.float32, [None, 784])
@@ -60,20 +85,23 @@ def main(_):
 
   sess = tf.InteractiveSession()
   tf.global_variables_initializer().run()
+
   # Train
-  for _ in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
+  print("Starting training...")
+  for i in range(1000):
+    batch_xs = next_batch(images, i*100, 100)
+    batch_ys = next_batch(labels, i*100, 100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
   # Test trained model
+  print("Training complete! Checking accuracy...")
   correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                      y_: mnist.test.labels}))
+  score = sess.run(accuracy, feed_dict={x: np.array(images), y_: np.array(labels)})
+  print("%2.2f%% of samples correctly labeled." % (100*score))
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
-                      help='Directory for storing input data')
+  parser.add_argument('--input_data', type=str, default='data.csv', help='Input data file')
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
